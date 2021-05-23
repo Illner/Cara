eec002584dca91f87f176dd7324e990e6a545faa
Petr Illner <illner3@gmail.com>
2021-05-23 21:11:57 +0200

    None

--- compiler/hypergraph_partitioning/patoh_data.py
+++ compiler/hypergraph_partitioning/patoh_data.py
@@ -53,7 +53,6 @@ class PatohData:
         self.__parameters._k = 2
         self.__parameters.seed = -1  # random seed
         self.__parameters.final_imbal = epsilon
-        self.__parameters.fast_initbal_mult = 1.5
 
     # region Public method
     def cwghts_ctypes(self) -> ctypes:
