7cc4a5e3a78172f6cb60de40bccc91682de0ff23
Petr Illner <illner3@gmail.com>
2021-07-04 10:55:32 +0200

    None

--- cara_experiment_2.py
+++ cara_experiment_2.py
@@ -64,8 +64,8 @@ def main(main_args):
             experiment.experiment(file_name=file_name, file_path=file_path,
 
                                   hp_cache_enum=hpc_enum.HypergraphPartitioningCacheEnum.ISOMORFISM,
-                                  hp_limit_number_of_clauses_cache=(None, 250),
-                                  hp_limit_number_of_variables_cache=(None, 250),
+                                  hp_limit_number_of_clauses_cache=(None, 500),
+                                  hp_limit_number_of_variables_cache=(None, 500),
                                   hp_multi_occurrence_cache=False,
 
                                   smooth=False,
