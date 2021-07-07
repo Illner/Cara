2a1e3d8f6332cde4104ad723ca7aecec977eba8c
Petr Illner <illner3@gmail.com>
2021-07-07 09:17:43 +0200

    None

--- cara_experiment.py
+++ cara_experiment.py
@@ -71,7 +71,7 @@ def main(main_args):
                                   decision_heuristic_vsads_p_constant_factor=1,
                                   decision_heuristic_vsads_q_constant_factor=0.5,
                                   imbalance_factor=0.25,
-                                  base_class_threshold=250,
+                                  base_class_threshold=500,
 
                                   smooth=False,
                                   preprocessing=False,
--- cara_experiment_2.py
+++ cara_experiment_2.py
@@ -13,6 +13,7 @@ import exception.cara_exception as c_exception
 
 # Import enum
 import compiler.enum.sat_solver_enum as ss_enum
+import compiler.enum.base_class_enum as bc_enum
 import compiler.enum.implied_literals_enum as il_enum
 import compiler.enum.component_caching_enum as cc_enum
 import compiler.enum.heuristic.decision_heuristic_enum as dh_enum
@@ -63,19 +64,20 @@ def main(main_args):
         try:
             experiment.experiment(file_name=file_name, file_path=file_path,
 
-                                  hp_cache_enum=hpc_enum.HypergraphPartitioningCacheEnum.ISOMORFISM_VARIANCE,
-                                  hp_limit_number_of_clauses_cache=(None, 1500),
-                                  hp_limit_number_of_variables_cache=(None, 1500),
-                                  hp_multi_occurrence_cache=False,
+                                  decision_heuristic_ignore_binary_clauses=False,
+
+                                  decision_heuristic_enum=dh_enum.DecisionHeuristicEnum.JEROSLOW_WANG_TWO_SIDED,
+                                  base_class_enum_set={bc_enum.BaseClassEnum.TWO_CNF, bc_enum.BaseClassEnum.RENAMABLE_HORN_CNF},
+                                  decision_heuristic_vsids_d4_version=True,
+                                  decision_heuristic_vsads_p_constant_factor=1,
+                                  decision_heuristic_vsads_q_constant_factor=0.5,
+                                  imbalance_factor=0.25,
 
                                   smooth=False,
                                   preprocessing=False,
-                                  imbalance_factor=None,
                                   subsumed_threshold=500,
                                   new_cut_set_threshold=0.1,
-                                  decision_heuristic_enum=dh_enum.DecisionHeuristicEnum.VSADS,
                                   sat_solver_enum=ss_enum.SatSolverEnum.MiniSAT,
-                                  base_class_enum_set=set(),
                                   implied_literals_enum=il_enum.ImpliedLiteralsEnum.BCP,
                                   implied_literals_preselection_heuristic_enum=ph_enum.PreselectionHeuristicEnum.NONE,
                                   first_implied_literals_enum=il_enum.ImpliedLiteralsEnum.BCP,
@@ -85,16 +87,16 @@ def main(main_args):
                                   component_caching_after_unit_propagation=True,
                                   eliminating_redundant_clauses_enum=erc_enum.EliminatingRedundantClausesEnum.NONE,
                                   eliminating_redundant_clauses_threshold=None,
+                                  hp_cache_enum=hpc_enum.HypergraphPartitioningCacheEnum.ISOMORFISM,
                                   hp_software_enum=hp_software_enum,
                                   hp_node_weight_type_enum=hpwt_enum.HypergraphPartitioningNodeWeightEnum.NONE,
                                   hp_hyperedge_weight_type_enum=hpwt_enum.HypergraphPartitioningHyperedgeWeightEnum.NONE,
                                   hp_variable_simplification_enum=hpvs_enum.HypergraphPartitioningVariableSimplificationEnum.EQUIV_SIMPL,
                                   hp_patoh_sugparam_enum=hpps_enum.PatohSugparamEnum.QUALITY,
-                                  decision_heuristic_vsids_d4_version=True,
-                                  decision_heuristic_vsads_p_constant_factor=1,
-                                  decision_heuristic_vsads_q_constant_factor=1,
+                                  hp_multi_occurrence_cache=True,
+                                  hp_limit_number_of_clauses_cache=(None, 500),
+                                  hp_limit_number_of_variables_cache=(None, 500),
                                   decision_heuristic_mixed_difference_enum=mdh_enum.MixedDifferenceHeuristicEnum.OK_SOLVER,
-                                  decision_heuristic_ignore_binary_clauses=False,
                                   decision_heuristic_weight_for_satisfied_clauses=True,
                                   component_caching_cara_caching_scheme_multi_occurrence=False,
                                   component_caching_cara_caching_scheme_basic_caching_scheme_number_of_variables_threshold=30
