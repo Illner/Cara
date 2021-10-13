6840b1265744f61a11dbf4ebc07b13225f31c498
„Petr <illner3@gmail.com>
2021-10-13 09:58:51 +0200

    None

--- tests/compiler/compiler/compiler_test.py
+++ tests/compiler/compiler/compiler_test.py
@@ -36,7 +36,7 @@ class CompilerTest(TestAbstract):
             print()
             print(f"File ({file_name}): ")
 
-            if file_name == "ais6.cnf":
+            if file_name == "anomaly.cnf":
                 self.__test(file_path)
 
         print()
