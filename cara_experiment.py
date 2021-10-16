12f3e0c24d191d9e21a91a6fe3fc9f55971a84ac
„Petr <illner3@gmail.com>
2021-10-16 15:18:53 +0200

    None

--- cara_experiment.py
+++ cara_experiment.py
@@ -64,17 +64,19 @@ def main(main_args):
         try:
             experiment.experiment(file_name=file_name, file_path=file_path,
 
-                                  decision_heuristic_enum=dh_enum.DecisionHeuristicEnum.CLAUSE_REDUCTION,
-                                  decision_heuristic_weight_for_satisfied_clauses=True,
-                                  disable_sat=True,
+                                  decision_heuristic_ignore_binary_clauses=True,
+                                  decision_heuristic_enum=dh_enum.DecisionHeuristicEnum.VSADS,
+                                  base_class_enum_set={bc_enum.BaseClassEnum.TWO_CNF, bc_enum.BaseClassEnum.RENAMABLE_HORN_CNF},
+                                  decision_heuristic_vsids_d4_version=True,
+                                  decision_heuristic_vsads_p_constant_factor=1,
+                                  decision_heuristic_vsads_q_constant_factor=0.5,
 
                                   smooth=False,
                                   preprocessing=False,
-                                  imbalance_factor=None,
+                                  imbalance_factor=0.1,
                                   subsumed_threshold=500,
                                   new_cut_set_threshold=0.1,
                                   sat_solver_enum=ss_enum.SatSolverEnum.MiniSAT,
-                                  base_class_enum_set=set(),
                                   implied_literals_enum=il_enum.ImpliedLiteralsEnum.BCP,
                                   implied_literals_preselection_heuristic_enum=ph_enum.PreselectionHeuristicEnum.NONE,
                                   first_implied_literals_enum=il_enum.ImpliedLiteralsEnum.BCP,
@@ -84,15 +86,19 @@ def main(main_args):
                                   component_caching_after_unit_propagation=True,
                                   eliminating_redundant_clauses_enum=erc_enum.EliminatingRedundantClausesEnum.NONE,
                                   eliminating_redundant_clauses_threshold=None,
-                                  hp_cache_enum=hpc_enum.HypergraphPartitioningCacheEnum.NONE,
-                                  hp_software_enum=hps_enum.HypergraphPartitioningSoftwareEnum.HMETIS if env.is_windows() else hps_enum.HypergraphPartitioningSoftwareEnum.PATOH,
+                                  hp_cache_enum=hpc_enum.HypergraphPartitioningCacheEnum.ISOMORFISM,
+                                  hp_software_enum=hp_software_enum,
                                   hp_node_weight_type_enum=hpwt_enum.HypergraphPartitioningNodeWeightEnum.NONE,
                                   hp_hyperedge_weight_type_enum=hpwt_enum.HypergraphPartitioningHyperedgeWeightEnum.NONE,
                                   hp_variable_simplification_enum=hpvs_enum.HypergraphPartitioningVariableSimplificationEnum.EQUIV_SIMPL,
                                   hp_patoh_sugparam_enum=hpps_enum.PatohSugparamEnum.QUALITY,
-                                  decision_heuristic_vsids_d4_version=True,
-                                  decision_heuristic_vsads_p_constant_factor=1,
-                                  decision_heuristic_vsads_q_constant_factor=1)
+                                  hp_multi_occurrence_cache=True,
+                                  hp_limit_number_of_clauses_cache=(None, 500),
+                                  hp_limit_number_of_variables_cache=(None, 500),
+                                  decision_heuristic_mixed_difference_enum=mdh_enum.MixedDifferenceHeuristicEnum.OK_SOLVER,
+                                  decision_heuristic_weight_for_satisfied_clauses=True,
+                                  component_caching_cara_caching_scheme_multi_occurrence=False,
+                                  component_caching_cara_caching_scheme_basic_caching_scheme_number_of_variables_threshold=30)
 
         except (c_exception.CaraException, Exception) as err:
             print(f"An error has occurred! (see log for details)\n{str(err)}")
