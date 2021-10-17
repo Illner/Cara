c1eafee49d5ff63fd2aebb9bff977aba324a15c2
„Petr <illner3@gmail.com>
2021-10-17 18:43:13 +0200

    None

--- cara_test.py
+++ cara_test.py
@@ -234,7 +234,7 @@ def create_parser() -> argparse.ArgumentParser:
     parser_temp.add_argument("-ct_l",
                              "--compiler_test_limit",
                              action="store",
-                             default=9600,
+                             default=384,
                              type=cara.non_negative_int_parser,
                              metavar="[non-negative number]",
                              help="maximum number of tests per file")
