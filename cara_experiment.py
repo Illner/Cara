46ab0a69a514f5c267c89d196b93f0b100758d8e
„Petr <illner3@gmail.com>
2021-10-29 09:04:19 +0200

    None

--- cara_experiment.py
+++ cara_experiment.py
@@ -37,7 +37,7 @@ def main(main_args):
                             directory_path=main_args.directory_path,
                             timeout_experiment=timeout_experiment,
                             log_directory_path=main_args.log_directory_path,
-                            save_circuit=False,
+                            save_circuit=True,
                             mapping_node_statistics=None,
                             node_statistics=None)
 
@@ -73,9 +73,9 @@ def main(main_args):
                                   decision_heuristic_vsads_p_constant_factor=1,
                                   decision_heuristic_vsads_q_constant_factor=0.5,
 
-                                  decision_heuristic_renamable_horn_use_total_number_of_conflict_variables=True,
-                                  decision_heuristic_renamable_horn_use_conflicts=False,
-                                  decision_heuristic_renamable_horn_prefer_conflict_variables=False,
+                                  decision_heuristic_renamable_horn_use_total_number_of_conflict_variables=False,
+                                  decision_heuristic_renamable_horn_use_conflicts=True,
+                                  decision_heuristic_renamable_horn_prefer_conflict_variables=True,
 
                                   smooth=False,
                                   preprocessing=False,
