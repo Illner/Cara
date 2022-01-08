e95a887add3a877aa288a120efd74060f422fc93
„Petr <illner3@gmail.com>
2022-01-08 19:33:08 +0100

    None

--- test.py
+++ test.py
@@ -27,8 +27,6 @@ for folder_name, folder_path in dir_list:
 
         stopwatch_time = sys_time.perf_counter_ns()
 
-        subprocess.run(["cat", "-e", experiment_path])
-
         output_path_temp = Path(os.path.join(output_path, experiment_name))
         subprocess.run(["./cnfprime", experiment_path, output_path_temp])
 
